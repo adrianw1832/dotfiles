6798c4b202356aea80dbee473a396eeb4ccd9a77
Adrian Wong <adrianw1832@hotmail.co.uk>
2015-10-09 01:17:59 +0100

    added zsh files

---
+++ zsh/aliases.zsh
@@ -0,0 +1,81 @@
+# Easier navigation
+alias ..="cd .."
+alias ...="cd ../.."
+alias ....="cd ../../.."
+alias .....="cd ../../../.."
+
+# Shortcuts
+# alias d="cd ~/Documents/Dropbox"
+alias dl="cd ~/Downloads"
+alias dt="cd ~/Desktop"
+alias p="cd ~/Desktop/projects"
+
+# Hidden files
+alias show="defaults write com.apple.finder AppleShowAllFiles -bool true && killall Finder"
+alias hide="defaults write com.apple.finder AppleShowAllFiles -bool false && killall Finder"
+
+# Clear
+alias c='clear'
+
+#Source Tree
+alias st="open -a SourceTree"
+
+# Ls
+alias ls='ls -GFh'
+export LSCOLORS=GxFxCxDxBxegedabagaced
+
+# Stopwatch
+alias timer='echo "Timer started. Stop with Ctrl-D." && date && time cat && date'
+
+# Get OS X Software Updates, and update installed Ruby gems, Homebrew, npm, and their installed packages
+alias update='softwareupdate -i -a; brew update; brew upgrade --all; brew cleanup; npm install npm -g; npm update -g; gem update --system; gem update'
+
+# Rails
+alias rr="bin/rake routes"
+alias rdbc="bin/rake db:create"
+alias rdbct="bin/rake db:create RAILS_ENV=test"
+alias rdbd="bin/rake db:drop"
+alias rdbm="bin/rake db:migrate"
+alias rdbmt="bin/rake db:migrate RAILS_ENV=test"
+alias rdbrb="bin/rake db:rollback"
+alias rdbr="bin/rake db:reset"
+alias rs="bin/rails server --binding=127.0.0.1"
+alias rc="bin/rails console"
+alias rgrspec="bin/rails generate rspec:install"
+alias rgc="bin/rails generate controller"
+alias rdc="bin/rails destroy controller"
+alias rgmo="bin/rails generate model"
+alias rdmo="bin/rails destroy model"
+alias rgmi="bin/rails generate migration"
+alias rdmi="bin/rails destroy migration"
+
+# copy the working directory path
+alias cpwd='pwd|tr -d "\n"|pbcopy'
+
+# editor shortcut
+alias v="vim"
+
+#Config Shortcuts
+alias za="vim ~/dotfiles/zsh/aliases.zsh"
+alias ze="vim ~/.oh-my-zsh/custom/exports.zsh"
+alias zf="vim ~/.oh-my-zsh/custom/functions.zsh"
+alias zshrc="vim ~/dotfiles/zsh/zshrc"
+alias vimrc="vim ~/dotfiles/vimrc"
+alias gc="vim ~/.gitconfig"
+
+#Testing Angular
+alias wds="webdriver-manager start"
+alias e2e="gulp e2e"
+alias test="gulp test"
+
+# Git
+alias g="git status"
+alias ga="git add"
+alias gc="git commit"
+alias go="git checkout"
+
+#Reload the shell
+alias reload="exec $SHELL -l"
+
+# Lock the screen (when going AFK)
+alias afk="/System/Library/CoreServices/Menu\ Extras/User.menu/Contents/Resources/CGSession -suspend"
