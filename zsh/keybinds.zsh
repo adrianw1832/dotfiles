5056408ee05000ecfad03ffa00295945514073ad
Adrian Wong <awong@thoughtworks.com>
2016-10-21 18:23:24 +0100

    Add keybinds mapping settings for zsh

---
+++ zsh/keybinds.zsh
@@ -0,0 +1,8 @@
+bindkey '^w' forward-word
+bindkey '^b' backward-word
+bindkey '^g' autosuggest-execute
+bindkey '^p' up-line-or-beginning-search
+bindkey '^n' down-line-or-beginning-search
+
+bindkey '^x^h' backward-kill-word
+
