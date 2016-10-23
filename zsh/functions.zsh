4f5f292a77b4269867817baeffbde6256722968c
Adrian Wong <awong@thoughtworks.com>
2016-10-24 00:20:02 +0100

    Add custom up function for smarter backwards cd

--- zsh/functions.zsh
+++ zsh/functions.zsh
@@ -1,4 +1,4 @@
-# Bind ctrl space to go back to background job
+# Custom function to go back to background job
 _easier_ctrl-z() {
   if [[ $#BUFFER -eq 0 ]]; then
     BUFFER="fg"
@@ -16,7 +16,26 @@ gac() { git add "$1" && git commit }
 # Custom function to handle git add, commit and push
 gacp() { git add "$1" && git commit && git push }
 
-# Always tmux is always running
+# Custom function to allow smarter backwards cd
+up() {
+  local op=print
+  [[ -t 1 ]] && op=cd
+  case "$1" in
+    '') up 1;;
+    -*|+*) $op ~$1;;
+    <->) $op $(printf '../%.0s' {1..$1});;
+    *) local -a seg; seg=(${(s:/:)PWD%/*})
+       local n=${(j:/:)seg[1,(I)$1*]}
+       if [[ -n $n ]]; then
+         $op /$n
+       else
+         print -u2 up: could not find prefix $1 in $PWD
+         return 1
+       fi
+  esac
+}
+
+# Ensure tmux is always running
 _not_inside_tmux() { [[ -z "$TMUX" ]] }
 
 ensure_tmux_is_running() {
