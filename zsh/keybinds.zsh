738f270da3298d840a18cc3eb1772ff573564eb4
Adrian Wong <awong@thoughtworks.com>
2016-10-23 01:42:05 +0100

    Add a bunch of vi mode keybindings

--- zsh/keybinds.zsh
+++ zsh/keybinds.zsh
@@ -1,8 +1,31 @@
-bindkey '^w' forward-word
-bindkey '^b' backward-word
-bindkey '^g' autosuggest-execute
-bindkey '^p' up-line-or-beginning-search
-bindkey '^n' down-line-or-beginning-search
+# Vi mode keybinds
+bindkey -M viins 'jk' vi-cmd-mode
+bindkey -M viins 'kj' vi-cmd-mode
 
-bindkey '^x^h' backward-kill-word
+bindkey -M viins '^o' autosuggest-execute
+bindkey -M viins '^p' up-line-or-beginning-search
+bindkey -M viins '^n' down-line-or-beginning-search
 
+bindkey -M viins '^b' backward-char
+bindkey -M viins '^f' forward-char
+
+bindkey -M viins '^d' delete-char
+bindkey -M viins '^g' backward-delete-char
+# bindkey -M viins '^h' backward-delete-char # It's blocked by tmux navigator
+
+bindkey -M viins '^r' history-incremental-search-backward
+bindkey -M viins '^t' history-incremental-search-forward
+
+bindkey -M vicmd 'Y' vi-yank-eol
+
+bindkey -M vicmd 'v' visual-mode
+bindkey -M vicmd 'V' visual-line-mode
+
+# Emacs-like keybinds
+# bindkey '^w' forward-word
+# bindkey '^b' backward-word
+# bindkey '^o' autosuggest-execute
+# bindkey '^p' up-line-or-beginning-search
+# bindkey '^n' down-line-or-beginning-search
+# bindkey '^xw' kill-word
+# bindkey '^xb' backward-kill-word
