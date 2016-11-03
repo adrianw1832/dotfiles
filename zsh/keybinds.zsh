33f06a1083428eee8f573ff5fa72289f8ba361ae
Adrian Wong <awong@thoughtworks.com>
2016-11-03 15:55:47 +0000

    Add some more emacs like key bindings

--- zsh/keybinds.zsh
+++ zsh/keybinds.zsh
@@ -8,13 +8,14 @@ bindkey -M viins '^n' down-line-or-beginning-search
 
 bindkey -M viins '^a' beginning-of-line
 bindkey -M viins '^e' end-of-line
+bindkey -M viins '^k' kill-line
+bindkey -M viins '^u' backward-kill-line
 
 bindkey -M viins '^b' backward-char
 bindkey -M viins '^f' forward-char
 
 bindkey -M viins '^d' delete-char
 bindkey -M viins '^h' backward-delete-char
-bindkey -M viins '^w' backward-kill-word
 
 # [Shift-Tab] - move through the completion menu backwards
 if [[ "${terminfo[kcbt]}" != "" ]]; then
@@ -26,18 +27,10 @@ bindkey -M viins '^ ' _easier_ctrl-z
 # bindkey -M viins '^r' history-incremental-pattern-search-backward
 # bindkey -M viins '^t' history-incremental-pattern-search-forward
 
+bindkey -M viins '^w' forward-word
 bindkey -M viins '^v' copy-prev-shell-word
 
 bindkey -M vicmd 'Y' vi-yank-eol
 
 bindkey -M vicmd 'v' visual-mode
 bindkey -M vicmd 'V' edit-command-line
-
-# Emacs-like keybinds
-# bindkey '^w' forward-word
-# bindkey '^b' backward-word
-# bindkey '^o' autosuggest-execute
-# bindkey '^p' up-line-or-beginning-search
-# bindkey '^n' down-line-or-beginning-search
-# bindkey '^xw' kill-word
-# bindkey '^xb' backward-kill-word
