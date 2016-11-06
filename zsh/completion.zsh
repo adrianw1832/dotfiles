f1ce8d4cceef42c17c2b1ac6a577d1bdd1249639
Adrian Wong <awong@thoughtworks.com>
2016-11-06 17:38:37 +0000

    Add messages and some settings for the completion

--- zsh/completion.zsh
+++ zsh/completion.zsh
@@ -3,7 +3,24 @@ autoload -U compinit
 compinit
 zmodload -i zsh/complist
 
+# Show coloured menu when tabbing
 zstyle ':completion:*' menu select
-# case-insensitive (all), partial-word and then substring completion
-zstyle ':completion:*' matcher-list 'm:{a-zA-Z}={A-Za-z}' 'r:|[._-]=* r:|=*' 'l:|=* r:|=*'
 zstyle ':completion:*' list-colors ''
+
+# Case-insensitive (all), partial-word and then substring completion
+zstyle ':completion:*' matcher-list 'm:{a-zA-Z}={A-Za-z}' 'r:|[._-]=* r:|=*' 'l:|=* r:|=*'
+
+# List of completers to use
+zstyle ':completion:*' completer _expand _complete
+
+# Formatting and messages
+zstyle ':completion:*' verbose yes
+zstyle ':completion:*:descriptions' format '%B%d%b'
+zstyle ':completion:*:messages' format '%d'
+zstyle ':completion:*:warnings' format 'No matches for: %d'
+zstyle ':completion:*:corrections' format '%B%d (errors: %e)%b'
+zstyle ':completion:*' group-name ''
+
+# Enable completion caching, use rehash to clear
+zstyle ':completion::complete:*' use-cache on
+zstyle ':completion::complete:*' cache-path ~/.zsh/cache
