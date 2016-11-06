1a105d41538212977e4b23cb723d8bde5d258a87
Adrian Wong <awong@thoughtworks.com>
2016-11-06 18:49:02 +0000

    Make sure that the colours are consistent

--- zsh/completion.zsh
+++ zsh/completion.zsh
@@ -1,11 +1,11 @@
 # Use modern completion system
-autoload -U compinit
-compinit
+autoload -U compinit && compinit
+autoload -U colors ; colors
 zmodload -i zsh/complist
 
 # Show coloured menu when tabbing
 zstyle ':completion:*' menu select
-zstyle ':completion:*' list-colors ''
+zstyle ':completion:*' list-colors 'di=1;36:ln=35:so=32:pi=33:ex=31:bd=34;46:cd=34;43:su=30;41:sg=30;46:tw=30;42:ow=30;43'
 
 # Case-insensitive (all), partial-word and then substring completion
 zstyle ':completion:*' matcher-list 'm:{a-zA-Z}={A-Za-z}' 'r:|[._-]=* r:|=*' 'l:|=* r:|=*'
@@ -14,11 +14,10 @@ zstyle ':completion:*' matcher-list 'm:{a-zA-Z}={A-Za-z}' 'r:|[._-]=* r:|=*' 'l:
 zstyle ':completion:*' completer _expand _complete
 
 # Formatting and messages
-zstyle ':completion:*' verbose yes
+zstyle ':completion:*' extra-verbose true
 zstyle ':completion:*:descriptions' format '%B%d%b'
 zstyle ':completion:*:messages' format '%d'
 zstyle ':completion:*:warnings' format 'No matches for: %d'
-zstyle ':completion:*:corrections' format '%B%d (errors: %e)%b'
 zstyle ':completion:*' group-name ''
 
 # Enable completion caching, use rehash to clear
