594be6b610d935765d5bc688a60e0f49a543cfc8
Adrian Wong <awong@thoughtworks.com>
2016-11-06 18:49:57 +0000

    Add vim movement keys to menu completion

--- zsh/keybinds.zsh
+++ zsh/keybinds.zsh
@@ -39,3 +39,9 @@ bindkey -M vicmd 'Y' vi-yank-eol
 autoload -U edit-command-line
 zle -N edit-command-line
 bindkey -M vicmd 'v' edit-command-line
+
+# Use vim navigation keys besides cursor keys in menu completion
+bindkey -M menuselect 'h' vi-backward-char
+bindkey -M menuselect 'k' vi-up-line-or-history
+bindkey -M menuselect 'l' vi-forward-char
+bindkey -M menuselect 'j' vi-down-line-or-history
