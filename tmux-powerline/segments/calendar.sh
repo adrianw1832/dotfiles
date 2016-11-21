15eb4e800748dcbca9faaf27febc6a8123ddb8b5
Adrian Wong <awong@thoughtworks.com>
2016-11-21 22:55:14 +0000

    Remove icons from calendar segment

--- tmux-powerline/segments/calendar.sh
+++ tmux-powerline/segments/calendar.sh
@@ -1,32 +1,19 @@
+# date(1) format for the time. Americans might want to have "%I:%M %p".
 TMUX_POWERLINE_SEG_TIME_FORMAT_DEFAULT="%H:%M"
+# date(1) format for the date. If you don't, for some reason, like ISO 8601 format you might want to have "%D" or "%m/%d/%Y".
 TMUX_POWERLINE_SEG_DATE_FORMAT_DEFAULT="%d-%m-%Y"
 
 generate_segmentrc() {
   read -d '' rccontents  << EORC
-  # date(1) format for the time. Americans might want to have "%I:%M %p".
-  export TMUX_POWERLINE_SEG_TIME_FORMAT="${TMUX_POWERLINE_SEG_TIME_FORMAT_DEFAULT}"
-  # date(1) format for the date. If you don't, for some reason, like ISO 8601 format you might want to have "%D" or "%m/%d/%Y".
-  export TMUX_POWERLINE_SEG_DATE_FORMAT="${TMUX_POWERLINE_SEG_DATE_FORMAT_DEFAULT}"
 EORC
   echo "$rccontents"
 }
 
-__process_settings() {
-  if [ -z "$TMUX_POWERLINE_SEG_TIME_FORMAT" ]; then
-    export TMUX_POWERLINE_SEG_TIME_FORMAT="${TMUX_POWERLINE_SEG_TIME_FORMAT_DEFAULT}"
-  fi
-  if [ -z "$TMUX_POWERLINE_SEG_DATE_FORMAT" ]; then
-    export TMUX_POWERLINE_SEG_DATE_FORMAT="${TMUX_POWERLINE_SEG_DATE_FORMAT_DEFAULT}"
-  fi
-}
-
 run_segment() {
-  __process_settings
+  day=$(date +"%a")
+  time=$(date +"$TMUX_POWERLINE_SEG_TIME_FORMAT_DEFAULT")
+  date=$(date +"$TMUX_POWERLINE_SEG_DATE_FORMAT_DEFAULT")
 
-	day=$(date +"%a")
-  time=$(date +"$TMUX_POWERLINE_SEG_TIME_FORMAT")
-  date=$(date +"$TMUX_POWERLINE_SEG_DATE_FORMAT")
-
-  echo "$day " "$time"  "$date"
+  echo "$day"  "$time"  "$date"
   return 0
 }
