45bdee5fe9efec89c580b1fd5aab80a3fe9b9a54
Adrian Wong <awong@thoughtworks.com>
2016-11-27 23:58:14 +0000

    Remove postgres app

--- zsh/exports.zsh
+++ zsh/exports.zsh
@@ -4,7 +4,6 @@ export KEYTIMEOUT=1
 export LSCOLORS=Gxfxcxdxbxegedabagacad
 export N_PREFIX=$HOME
 export PATH=$PATH:$HOME/bin
-export PATH=$PATH:/Applications/Postgres.app/Contents/Versions/latest/bin
 export SHELL='/usr/local/bin/zsh'
 export WORDCHARS='*?_-.[]~=&;!#$%^(){}<>'
 
