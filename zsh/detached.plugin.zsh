cf15bb1546bfe3df4f3828f4d3f30612c15b28b7
Adrian Wong <awong@thoughtworks.com>
2017-08-12 01:38:03 +0100

    Patch git powerline to properly display tags

--- zsh/detached.plugin.zsh
+++ zsh/detached.plugin.zsh
@@ -17,3 +17,28 @@ function +vi-git-remotebranch() {
       hook_com[branch]+="$(print_icon 'VCS_REMOTE_BRANCH_ICON')${remote// /}"
   fi
 }
+
+function +vi-git-tagname() {
+    if [[ "$POWERLEVEL9K_VCS_HIDE_TAGS" == "false" ]]; then
+        # If we are on a tag, append the tagname to the current branch string.
+        local tag
+        tag=$(git describe --tags --exact-match HEAD 2>/dev/null)
+
+        if [[ -n "${tag}" ]] ; then
+            # There is a tag that points to our current commit. Need to determine if we
+            # are also on a branch, or are in a DETACHED_HEAD state.
+            if [[ -z $(git symbolic-ref HEAD 2>/dev/null) ]]; then
+                # DETACHED_HEAD state. We want to append the tag name to the commit hash
+                # and print it. Unfortunately, `vcs_info` blows away the hash when a tag
+                # exists, so we have to manually retrieve it and clobber the branch
+                # string.
+                local revision
+                revision=$(git rev-list -n 1 --abbrev-commit --abbrev=${POWERLEVEL9K_VCS_INTERNAL_HASH_LENGTH} HEAD)
+                hook_com[branch]="  ${revision} $(print_icon 'VCS_TAG_ICON') ${tag}"
+            else
+                # We are on both a tag and a branch; print both by appending the tag name.
+                hook_com[branch]+=" $(print_icon 'VCS_TAG_ICON') ${tag}"
+            fi
+        fi
+    fi
+}
