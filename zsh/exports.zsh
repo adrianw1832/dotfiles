fa7ed505da2d31e4af98a7dee617cc2fe9c6ad53
Adrian Wong <awong@thoughtworks.com>
2017-08-21 16:28:51 +0100

    More comprehensive Rg command for file search

--- zsh/exports.zsh
+++ zsh/exports.zsh
@@ -1,10 +1,12 @@
 export EDITOR='/usr/local/bin/nvim'
-export FZF_DEFAULT_COMMAND='rg --files -S'
 export KEYTIMEOUT=1
 export LSCOLORS=Gxfxcxdxbxegedabagacad
 export N_PREFIX=$HOME
 export SHELL='/usr/local/bin/zsh'
 export WORDCHARS='*?_-.[]~=&;!#$%^(){}<>'
+
+export FZF_DEFAULT_COMMAND='rg --files --no-ignore --hidden --follow -g "!{.git,node_modules}/*" 2> /dev/null'
+export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
 
 # Remove duplicate entries
 typeset -U PATH
