dd782ca73d2abdb1846a2ee1fb44c760777e6102
Adrian Wong <awong@thoughtworks.com>
2017-08-21 10:51:57 +0100

    Add better defaults for git add, show and pull
    Also added folding to functions for better readability

--- zsh/functions.zsh
+++ zsh/functions.zsh
@@ -1,4 +1,4 @@
-# Custom function to go back to background job
+# Custom function to go back to background job# {{{
 _easier_ctrl-z() {
 if [[ $#BUFFER -eq 0 ]]; then
   BUFFER="fg"
@@ -9,18 +9,13 @@ else
 fi
 }
 zle -N _easier_ctrl-z
-
-# Show all the modified files in the editor
+# }}}
+# Show all the modified files in the editor# {{{
 dirty() {
   git status | grep modified | awk '{print $2}' | xargs $EDITOR
 }
-
-# Find all local branches that are merged except for master and dev(elop) and delete them
-deletelocalmergedbranches() {
-  git branch --merged | egrep -v "(^\*|master|develop|dev)" | xargs git branch -d
-}
-
-# Get latest from master and then rebase master on current branch
+# }}}
+# Get latest from master and then rebase master on current branch# {{{
 grbm() {
   local currentbranch=$(git branch | grep \* | cut -d ' ' -f2)
   git checkout master
@@ -28,25 +23,33 @@ grbm() {
   git checkout $currentbranch
   git rebase master
 }
-
-# Custom function to handle git add and commit
+# }}}
+# Git add adds all if no arguments are specified# {{{
+ga() { git add "${1:-.}" }
+compdef _git ga=git-add
+# }}}
+# Git add and then commit# {{{
 gac() { git add "${1:-.}" && git commit --verbose }
 compdef _git gac=git-add
-
-# Custom function to handle git add, commit and push
+# }}}
+# Git add, commit and then push# {{{
 gacp() { git add "${1:-.}" && git commit && git push }
 compdef _git gacp=git-add
-
-# Custom function to handle git diff, for an optional argument
+# }}}
+# Git show for all if no arugments are specified# {{{
+gsh() { git show --color=always "${1:-.}" | diff-so-fancy | less -CGR }
+compdef _git gd=git-show
+# }}}
+# Git diff for all if not arguments are specified# {{{
 gd() { git diff --color=always "${1:-.}" | diff-so-fancy | less -CGR }
 compdef _git gd=git-diff
-
-# Custom git log and defaults to showing 15 results unless an argument is given
+# }}}
+# Custom git log and defaults to showing 15 results unless an argument is given# {{{
 glog() {
   git log --color=always --date=format:%a\ %H:%M\ %d-%m-%Y --format=log "${1:--15}"
 }
-
-# Pass git log to fzf and can check the diff of the commit object
+# }}}
+# Pass git log to fzf and can check the diff of the commit object# {{{
 gitlog() {
   glog "${1:---all}" | fzf --ansi --exact --no-sort --reverse --tiebreak=index \
     --bind "ctrl-m:execute:
@@ -55,37 +58,44 @@ gitlog() {
             {}
             FZF-EOF"
 }
-
-# Custom ref log and defaults to showing 15 results unless an argument is given
+# }}}
+# Custom ref log and defaults to showing 15 results unless an argument is given# {{{
 rlog() {
   git reflog --format=reflog "${1:--15}"
 }
-
-# Pass ref log to fzf
+# }}}
+# Pass ref log to fzf# {{{
 reflog() {
   rlog "${1:--250}" | fzf --ansi --exact --no-sort --reverse --tiebreak=index \
     | grep -o 'HEAD@{\d\+}' | xargs git checkout
 }
-
-# Get the commit sha - example usage: git rebase -i `fcs`
+# }}}
+# Get the commit sha - example usage: git rebase -i `fcs`# {{{
 fcs() {
   glog "${1:---all}" | fzf --ansi --exact --no-sort --reverse --tiebreak=index \
     | grep -o '[a-f0-9]\{7\}'
 }
-
+# }}}
+# Git push sets up origin if it is not already set, otherwise normal git push# {{{
 gp() {
   local branch=$(git rev-parse --abbrev-ref HEAD)
   local remote=$(git config branch.$branch.merge)
   [[ $remote = '' ]] && git push -u origin $branch || git push
 }
 compdef _git gp=git-push
-
-deletebranch() {
+# }}}
+# Find all local branches that are merged except for master and dev(elop) and delete them# {{{
+deletelocalmergedbranches() {
+  git branch --merged | egrep -v "(^\*|master|develop|dev)" | xargs git branch -d
+}
+# }}}
+# Delete both the local and remote branch# {{{
+deletelocalandremotebranch() {
   git branch -d $1 && git push -d origin $1
 }
 compdef _git deletebranch=git-branch
-
-# Custom function to allow smarter backwards cd
+# }}}
+# Smarter backwards cd# {{{
 up() {
   local op=print
   [[ -t 1 ]] && op=cd
@@ -103,8 +113,8 @@ up() {
       fi
   esac
 }
-
-# Custom function to display the path in a better way
+# }}}
+# Display paths in a better way# {{{
 path() {
   echo $PATH | tr ":" "\n" | \
     awk "{ sub(\"/usr\",     \"$fg_no_bold[green]/usr$reset_color\"); \
@@ -117,20 +127,23 @@ path() {
     sub(\"/.rbenv\",  \"$fg_no_bold[red]/.rbenv$reset_color\"); \
     print }"
 }
-
-# Custom function to display the env in a better way
+# }}}
+# Display env in a better way# {{{
 env() {
   printenv | \
     awk "{ sub(\"=\", \"$fg_no_bold[red]=$reset_color\"); \
     print }"
 }
-
+# }}}
+# Passing fasd directories to fzf# {{{
 jj() {
   local dir
   dir="$(fasd -Rdl "$1" | fzf -1 -0 --no-sort +m)" && cd "${dir}" || return 1
 }
-
+# }}}
+# Passing fasd files to fzf# {{{
 vv() {
   local file
   file="$(fasd -Rfl "$1" | fzf -1 -0 --no-sort +m)" && $EDITOR "${file}" || return 1
 }
+# }}}
