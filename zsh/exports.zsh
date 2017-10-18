cfa172a054db24598ab344dd74bd16a9be4544fd
Adrian Wong <awong@thoughtworks.com>
2017-10-18 16:19:26 +0100

    Add default options for fzf in command line

--- zsh/exports.zsh
+++ zsh/exports.zsh
@@ -5,6 +5,7 @@ export N_PREFIX=$HOME
 export SHELL='/usr/local/bin/zsh'
 export WORDCHARS='*?_-.[]~=&;!#$%^(){}<>'
 
+export FZF_DEFAULT_OPTS='--height 40% --reverse --border'
 export FZF_DEFAULT_COMMAND='rg --files --no-ignore --hidden --follow -g "!{.git,node_modules}/*" 2> /dev/null'
 export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
 
