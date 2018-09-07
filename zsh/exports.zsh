f98733c3b1d8638bb791ffee1ce54ca17977b6eb
Adrian Wong <awong@thoughtworks.com>
2018-09-07 17:13:06 +0100

    Add alternative to fzf search utility

--- zsh/exports.zsh
+++ zsh/exports.zsh
@@ -7,6 +7,7 @@ export WORDCHARS='*?_-.[]~=&;!#$%^(){}<>'
 
 export FZF_DEFAULT_OPTS='--height 40% --reverse --border'
 export FZF_DEFAULT_COMMAND='rg --files --no-ignore --hidden --follow -g "!{.git,node_modules}/*" 2> /dev/null'
+# export FZF_DEFAULT_COMMAND='ag -l --follow'
 export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
 
 # Remove duplicate entries
